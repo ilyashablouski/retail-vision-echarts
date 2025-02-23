2ffce7f23bc6fb44bf6898054de2e936030fb6ed
i.shablovskiy <ilya.shablovskiy@gmail.com>
2025-02-23 15:15:50 +0300

    edit file structure fit to FSD

---
+++ src/app/ui/App.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+// import reactLogo from '@assets/react.svg';
+// import viteLogo from '/vite.svg';
+
+const App: FC = () => {
+  return <></>;
+};
+
+export default App;
