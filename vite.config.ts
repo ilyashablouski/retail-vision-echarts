d603a05163e2294f967164faba23594ea61407d4
i.shablovskiy <ilya.shablovskiy@gmail.com>
2025-02-23 14:54:16 +0300

    add correct aliases fit to FSD

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 
 // https://vite.dev/config/
-
 const root = resolve(__dirname, 'src');
 
 export default defineConfig({
@@ -11,8 +10,13 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': resolve(root),
+      '@app': resolve(root, 'app'),
       '@assets': resolve(root, 'assets'),
-      '@components': resolve(root, 'components'),
+      '@entities': resolve(root, 'entities'),
+      '@features': resolve(root, 'features'),
+      '@pages': resolve(root, 'pages'),
+      '@shared': resolve(root, 'shared'),
+      '@widgets': resolve(root, 'widgets'),
     },
   },
 });
